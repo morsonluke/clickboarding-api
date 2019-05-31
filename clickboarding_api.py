ebe934bce910c912df5e1425cc446e852830bef6
Luke Morson <morsonluke@gmail.com>
2019-05-31 09:19:20 +0100

    update style

--- clickboarding_api.py
+++ clickboarding_api.py
@@ -1,16 +1,18 @@
-import http.client, urllib.request, urllib.parse, urllib.error, base64
-import ast
+import http.client
+import urllib.request
+import urllib.parse
+import urllib.error
 import json
 
 client_id = ''
 
 endpoints = {
     "clients": "/v1/Clients?",
-    "users"  : "/v1/Clients/" + client_id + "/Users?",
+    "users": "/v1/Clients/" + client_id + "/Users?",
     "candidates": "/v1/Clients/" + client_id + "/Candidates?",
     "locations": "/v1/Clients/" + client_id + "/Locations?",
     "processes": "/v1/Clients/" + client_id + "/Processes?",
-    "candidateprocesses" : "/v1/Clients/" + client_id + "/CandidateProcesses?",
+    "candidateprocesses": "/v1/Clients/" + client_id + "/CandidateProcesses?",
     "tasks": "/v1/Clients/" + client_id + "/Tasks?"
 }
 
@@ -22,35 +24,35 @@ class Clickboarding(object):
 
         for key, value in kwargs.items():
             setattr(self, key, value)
-    
+
     def call_api(self, uri, limit):
 
         headers = {
-        # Request headers
-        'x-api-key': '',
+            # Request headers
+            'x-api-key': '',
         }
 
         params = urllib.parse.urlencode({
-        # Request parameters
-        'limit': limit
+            # Request parameters
+            'limit': limit
         })
 
         try:
             conn = http.client.HTTPSConnection('api.clickboarding.com')
-            conn.request("GET", "%s%s" %(uri, params), "{body}", headers)
+            conn.request("GET", "%s%s" % (uri, params), "{body}", headers)
             response = conn.getresponse()
             data = response.read()
             conn.close()
         except Exception as e:
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
-
         # decode to str
         my_str = data.decode('utf8')
-        # decode str to dict 
+        # decode str to dict
         my_dict = json.loads(my_str)
         print(uri, len(my_dict['items']))
 
+
 for key, value in endpoints.items():
-   api = Clickboarding(key)
-   api.call_api(value, "1000")
+    api = Clickboarding(key)
+    api.call_api(value, "1000")
